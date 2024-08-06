df058c417e79273ecb3eb11e41d2012ea899749d
vrinch <odukwevince76@gmail.com>
2024-08-06 20:52:08 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className='container'>
+      <h1 className='heading'>
+        Web App Dev Starter Pack (Next.js, TypeScript, Tailwind CSS, and React)
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
